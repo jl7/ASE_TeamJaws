acc84491f0c5f98462ab5e40c66e5aa74c1d405a
du48s03 <u5b890402@gmail.com>
2014-11-07 17:10:42 -0500

    partly implemented model.py and champmodel.py

---
+++ LeagueAnalyzer/champmodel.py
@@ -0,0 +1,19 @@
+import abc
+from abc_base import EntityModel
+#import views
+
+class ChampionModel(EntityModel):
+    """docstring for SummonerModel"""
+    def __init__(self, arg):
+        super(SummonerModel, self).__init__()
+        self.arg = arg
+
+    def _setAllWithoutUpdate(self, data):
+        """Sets all attributes without calling the view's updateDisplay method"""
+        super(SummonerModel, self)._setAllWithoutUpdate(data)
+
+    def _setAll(self, data):
+        """Sets all attributes and call the view's updateDisplay method"""
+        super(SummonerModel, self)._setAll(data)
+
+    
